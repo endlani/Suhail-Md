77ee1d1fd6570ca46db32d53e9ac5c18ded099b4
endlani <148375938+endlani@users.noreply.github.com>
2024-03-23 03:33:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ global.disablegroup = process.env.DISABLE_GROUPS || "false", // disable bot in g
 
 global.MsgsInLog = process.env.MSGS_IN_LOG|| "false" // "true"  to see messages , "log" to open logs , "false" to hide logs messages
 global.userImages= process.env.USER_IMAGES || "https://telegra.ph/file/758549f0de2219884f387.jpg,https://telegra.ph/file/9498496b010dfb5900b80.jpg,https://telegra.ph/file/59bf80efbb6e6576b5a52.jpg,https://telegra.ph/file/ee0e191363161e88e4743.jpg" // ramadan Theme Images
-global.waPresence= process.env.WAPRESENCE ||  "set according to your need" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
+global.waPresence= process.env.WAPRESENCE ||  "unavailable" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
 
 
 //========================= [ AUTO READ MSGS & CMDS ] =========================\\
@@ -53,8 +53,8 @@ global.readmessagefrom = process.env.READ_MESSAGE_FROM || "923184474176,923xxxxx
 
 
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
-global.read_status = process.env.AUTO_READ_STATUS || "false"
-global.save_status = process.env.AUTO_SAVE_STATUS || "false"
+global.read_status = process.env.AUTO_READ_STATUS || "true"
+global.save_status = process.env.AUTO_SAVE_STATUS || "true"
 global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
 global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923184474176,923xxxxxxxx";
 
@@ -62,7 +62,7 @@ global.api_smd = "https://api-smd-1.vercel.app"
 global.scan = "https://suhail-md-vtsf.onrender.com/";
 
 
-global.SESSION_ID = process.env.SESSION_ID ||  "" ;
+global.SESSION_ID = process.env.SESSION_ID ||  "SUHAIL_18_21_02_23_ewogICJjcmVkcy5qc29uIjogIntcIm5vaXNlS2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiK01OeStwQ2M2Nk9KUVpDcW5obGJoTy8rZzFQMHV1Ym9nTmpXeXkrc1JFMD1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcImwvb3VqaE5KbVlma2dDYXJRak1zQWY4M1h0M210eVA1b3gwNDZVcS9weTQ9XCJ9fSxcInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyXCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiS0JIT3k4SWRBTUVRc2tGeHdHY2tSZGY2aDdKeDQySFRYcHpUMFRTYTgxST1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcImVTK051VFFyN09JeXJnc0YweVNmU1krcStXQ1hOaXdwbEg5R3pWKzBBU1E9XCJ9fSxcInNpZ25lZElkZW50aXR5S2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiWUhZSDZINEtmT0FvamcrZW9MS2NkZHh5WS9sS25kVTBXNmhVdVJ6V25Gcz1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIk9vMko0bW90b2lrR1cvM0R0d3NaWWdpck0wRDRldng4M2F2VDhLbGVzd0U9XCJ9fSxcInNpZ25lZFByZUtleVwiOntcImtleVBhaXJcIjp7XCJwcml2YXRlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJrRldqNCtoWDgyZGlyM0tja3ZHNHhmZ0h1KzUzTG1yc1JIUTgvZXBjQjFVPVwifSxcInB1YmxpY1wiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiSG1MOHBCSitvZ1J5amZNZVB1aGREL3Q0elYrQ29XeGovbllwbGNRY2VBOD1cIn19LFwic2lnbmF0dXJlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJlbnNXcnkwWlk4QVU0dytaY0E2a296MmlaZDN5VlF4aVM4NnhUUUorTU1YOUlrc2dYRHN4VXBaRjkrSTNpNUNJV2RvZE12WHRBWGpFSmY2OHFpdXNEZz09XCJ9LFwia2V5SWRcIjoxfSxcInJlZ2lzdHJhdGlvbklkXCI6MTI4LFwiYWR2U2VjcmV0S2V5XCI6XCIyQ01ldlhaNmIwcUc0czJsMHd1cWRGd25Ha1BFRHZxaEVqbEJzM2FvZVlzPVwiLFwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzXCI6W10sXCJuZXh0UHJlS2V5SWRcIjozMSxcImZpcnN0VW51cGxvYWRlZFByZUtleUlkXCI6MzEsXCJhY2NvdW50U3luY0NvdW50ZXJcIjowLFwiYWNjb3VudFNldHRpbmdzXCI6e1widW5hcmNoaXZlQ2hhdHNcIjpmYWxzZX0sXCJkZXZpY2VJZFwiOlwiUkZxQUJWTERRaENwanF3UkhQWDBhZ1wiLFwicGhvbmVJZFwiOlwiZjZhODI4NjItM2VhYy00Y2EzLWFjMmUtMzEyYWU0MmE2OTIxXCIsXCJpZGVudGl0eUlkXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJDU3lPVWtmeFFiWmIwQmx3Y0JwZ3loWUc0NlU9XCJ9LFwicmVnaXN0ZXJlZFwiOmZhbHNlLFwiYmFja3VwVG9rZW5cIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIjZsMHdLY0dHVFpnWGM4Q2xvNE1pMzhsOVV4UT1cIn0sXCJyZWdpc3RyYXRpb25cIjp7fSxcImFjY291bnRcIjp7XCJkZXRhaWxzXCI6XCJDTysvaUtJRUVKSEU0NjRHR0FFZ0FDZ0FcIixcImFjY291bnRTaWduYXR1cmVLZXlcIjpcIlZRWEpkc0NodzNPTXMyTjNOOUhRdUc0dWpkZmRtbFJhSUxiSDk1dUlnMkU9XCIsXCJhY2NvdW50U2lnbmF0dXJlXCI6XCJmdi9yalFvdkNYQU5vSStIc1BuS2VzcHV4endlNGdoTzJpcE9rTG1Rc2pCa3RyZjI4U3NwTmpJV2NLQTdVczlhczRQT1FkOVNtd2U1ZjREZ2lnQ2xEQT09XCIsXCJkZXZpY2VTaWduYXR1cmVcIjpcIlliRkhObEo2MCtOSUlNSmJrdmZTUzFSa3NONzhCbUtJazlucytPTEdwakR3WnNjMG5TN0VnT3kya09IUEozVHRjRlZ5RnFKQURSTzVYUWJlVFJtV0JnPT1cIn0sXCJtZVwiOntcImlkXCI6XCIyNjM3MTYwOTE1MDk6MzFAcy53aGF0c2FwcC5uZXRcIixcImxpZFwiOlwiMjAxNzkwODM0NDAxMzQ2OjMxQGxpZFwifSxcInNpZ25hbElkZW50aXRpZXNcIjpbe1wiaWRlbnRpZmllclwiOntcIm5hbWVcIjpcIjI2MzcxNjA5MTUwOTozMUBzLndoYXRzYXBwLm5ldFwiLFwiZGV2aWNlSWRcIjowfSxcImlkZW50aWZpZXJLZXlcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIkJWVUZ5WGJBb2NOempMTmpkemZSMExodUxvM1gzWnBVV2lDMngvZWJpSU5oXCJ9fV0sXCJwbGF0Zm9ybVwiOlwiYW5kcm9pZFwiLFwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wXCI6MTcwODcxMjQ3MH0iCn0=" ;
 
 
 module.exports = {
@@ -72,12 +72,12 @@ module.exports = {
   HANDLERS: process.env.PREFIX  || ".",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "1.3.1",
-  caption : process.env.CAPTION || "©sᴜʜᴀɪʟ²²¹-ᴍᴅ" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
+  caption : process.env.CAPTION || "*Mr Scientist Technogy Ai*" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
  
-  author : process.env.PACK_AUTHER|| "Suhail-MD",
+  author : process.env.PACK_AUTHER|| "Tsitneisc.m_r",
   packname: process.env.PACK_NAME || "♥️",
-  botname : process.env.BOT_NAME  || "sᴜʜᴀɪʟ-ᴍᴅ",
-  ownername:process.env.OWNER_NAME|| "It'x Suhail",
+  botname : process.env.BOT_NAME  || "Tsitneisc.m_r",
+  ownername:process.env.OWNER_NAME|| "It' Ehlani",
 
 
   errorChat : process.env.ERROR_CHAT || "",
@@ -92,8 +92,8 @@ module.exports = {
 
   aitts_Voice_Id : process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY  || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "private",
-  LANG: ( process.env.THEME ||  "SUHAIL"  ).toUpperCase(),
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "puplic",
+  LANG: ( process.env.THEME ||  "FRIDAY"  ).toUpperCase(),
 
 
 
